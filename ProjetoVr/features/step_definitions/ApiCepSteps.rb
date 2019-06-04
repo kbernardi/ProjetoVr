f9a7a5ff1d56d6d67e1aa05bd255f4bbee3028b5
kbernardi <51369463+kbernardi@users.noreply.github.com>
2019-06-04 09:35:05 -0300

    Add files via upload
    Inclusão das features e conf.

---
+++ ProjetoVr/features/step_definitions/ApiCepSteps.rb
@@ -0,0 +1,20 @@
+Quando("Realizo um Get na API dos correios com o CEP {string} válido.") do |cep|
+    @resposta =  HTTParty.get("https://viacep.com.br/ws/#{cep}/json")
+end
+
+Então("Valido o retorno da API.") do
+  expect(@resposta.code).to eql 200
+end
+
+Então("exibo o código do IBGE.") do
+  puts 'Código IBGE: ' + @resposta.parsed_response['ibge']
+end
+
+Quando("Realizo um Get na API dos correios com o CEP {string} invalido.") do |cep|
+  @resposta =  HTTparty.get("https://viacep.com.br/ws/#{cep}/json")
+end
+
+Então("Valido a mensagem de erro do retorno da API.") do
+  expect(@resposta.body).to eql '<h3>ViaCEP Bad Request (400)</h3>'
+  expect(@resposta.code).to eql 400
+end
